96ee1a858ab619fac996818fa58a99a6a719e0d6
Terence Huynh <me@terencehuynh.com>
2017-07-19 19:20:36 +1000

    Add toggling

--- cinder/js/base.js
+++ cinder/js/base.js
@@ -5,16 +5,15 @@ $( document ).ready(function() {
     $('table').addClass('table table-striped table-hover');
 });
 
-
 $('body').scrollspy({
     target: '.bs-sidebar',
 });
 
+$(document).on('click', "[data-toggle='rst-current-version']", function() {
+  $("[data-toggle='rst-versions']").toggleClass("shift-up");
+});
 
 /* Prevent disabled links from causing a page reload */
 $("li.disabled a").click(function() {
     event.preventDefault();
 });
-
-
-
